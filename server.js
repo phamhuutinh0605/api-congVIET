495df6701568de916a88bf9d85ad63a824b7cd99
huutinh <phamhuutinh801@gmail.com>
2023-06-22 15:03:53 +0700

    đá

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
   return res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(8800, () => {
+app.listen(process.env.PORT, () => {
   connect();
   console.log("Backend server is running!");
 });
